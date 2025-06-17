30378f12ef802aca8abdb2001123819ba4202da8
PabloReskPessot <pabloreskpessot@gmail.com>
2025-06-17 15:35:54 -0300

    Add files via upload

--- PIII_TrabajoFinal_BilleteraVirtual_FRONT/main.js
+++ PIII_TrabajoFinal_BilleteraVirtual_FRONT/main.js
@@ -11,3 +11,41 @@ function llamarAPI ()
     .then(data => console.log("Respuesta del GET:", data))
     .catch(err => console.error("Error en el GET:", err));
 }
+
+function llamarAPIget() {
+    fetch("http://localhost:5017/api/ppp")
+    .then(response => response.json())
+    .then(data => {
+        console.log("Datos recibidos:", data);
+    })
+    .catch(error => {
+        console.error("Error al llamar a la API:", error);
+    });
+}
+
+function header ()
+{
+    fetch("Header.html").then(response => response.text()).then(data => {
+        document.getElementById("header-container").innerHTML = data;
+      });
+}
+
+async function exchange(coin, fiat, volumen,operacion){
+    const res = await fetch(`https://criptoya.com/api/binance/${coin}/${fiat}/${volumen}`);
+    const data = await res.json();
+    if (operacion === "compra")
+    {
+        console.log(data.ask)
+        return data.ask;
+    };
+    if (operacion === "venta")
+    {
+        console.log(data.bid)
+        return data.bid;
+    };
+    //.then(data => {console.log("contacto", data)})
+}
+
+async function Cuenta(params) {
+    
+}
